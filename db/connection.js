f913b4b2b22eea1d5087353424360fb9f4bae27a
wonton1979 <guanyejun79@gmail.com>
2025-03-11 22:40:46 +0000

    Setting Up Hosting

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,22 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development'
 
+const config = {};
+
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+}
+
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
-
+module.exports = new Pool(config);
 module.exports = db;
--- listen.js
+++ listen.js
@@ -1,10 +1,10 @@
 const app = require("./app");
 
-app.listen(8080,(err)=>{
+app.listen(9090,(err)=>{
     if(err){
         console.log(err);
     }
     else{
-        console.log("Server Listening on port 8080");
+        console.log("Server Listening on port 9090");
     }
 });
